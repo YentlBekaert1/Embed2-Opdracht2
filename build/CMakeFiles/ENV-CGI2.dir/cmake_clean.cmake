b5455fdaf3a470f14f37ace155622dc4135c873f
YentlBekaert1 <yentl.bekaert@student.vives.be>
2021-10-05 09:35:02 +0200

    initial commit

---
+++ build/CMakeFiles/ENV-CGI2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ENV-CGI2.dir/env-cgi2.c.o"
+  "ENV-CGI2"
+  "ENV-CGI2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ENV-CGI2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
