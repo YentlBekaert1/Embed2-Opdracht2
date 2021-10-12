9f80c96279b71d776891565327209ba354f53f06
YentlBekaert1 <yentl.bekaert@student.vives.be>
2021-10-12 09:51:34 +0200

    display post data

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 sudo mv ENV-CGI2 /usr/lib/cgi-bin/ENV-CGI2
 sudo chmod a+rx /usr/lib/cgi-bin/ENV-CGI2
+
+HTMl en JSON File is /var/www/html plaatsen
--- env-cgi2.c
+++ env-cgi2.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DATAFILE "data/data.txt"
+
+#define MAXLEN 80
+#define EXTRA 5
+/* 4 for field name "data", 1 for "=" */
+#define MAXINPUT MAXLEN+EXTRA+2
+
+
+void unencode(char *src, char *last, char *dest)
+{
+ for(; src != last; src++, dest++)
+   if(*src == '+')
+     *dest = ' ';
+   else if(*src == '%') {
+     int code;
+     if(sscanf(src+1, "%2x", &code) != 1) code = '?';
+     *dest = code;
+     src +=2; }     
+   else
+     *dest = *src;
+ *dest = '\n';
+ *++dest = '\0';
+}
+
 int main(void)
 {
-FILE *f = fopen(DATAFILE,"r");
-int ch;
-if(f == NULL) {
-  printf("%s%c%c\n",
-  "Content-Type:text/html;charset=iso-8859-1",13,10);
-  printf("<TITLE>Failure</TITLE>\n");
-  printf("<P><EM>Unable to open data file, sorry!</EM>"); }
-else {
-  printf("%s%c%c\n",
-  "Content-Type:text/plain;charset=iso-8859-1",13,10);
-  while((ch=getc(f)) != EOF)
-    putchar(ch);
-  fclose(f); }
-return 0;
+  char *lenstr;
+  char input[MAXINPUT], data[MAXINPUT];
+  long len;
+  printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1",13,10);
+  printf("<TITLE>Response</TITLE>\n");
+  lenstr = getenv("CONTENT_LENGTH");
+  sscanf(lenstr,"%ld",&len)!=1;
+  fgets(input, len+1, stdin);
+  unencode(input+EXTRA, input+len, data);
+  printf("%s",data);
+  return 0;
 }
